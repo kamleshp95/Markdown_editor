29fc52507b6653b405e14436d28704f5a42e86fd
kamleshp95 <kamleshpadwal@gmail.com>
2021-07-20 21:00:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Markdownreact from './markdown/markdownreact';
+// import Markdown from "./markdown/markdown";
+
+function App() {
+  return (
+    <div className="App">
+      <Markdownreact />
+    </div>
+  );
+}
+
+export default App;
